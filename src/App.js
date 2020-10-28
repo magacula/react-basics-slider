d71e1c609a0676d566d949934db9da2a3d6cbe0e
Koi <Makoi.Gacula@student.csulb.edu>
2020-10-27 19:38:06 -0700

    commited project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import data from "./data";
 
 function App() {
+  // sets up State Hook to return an array of people
+  const [people, setPeople] = useState(data);
+
+  // set up State Hook to get index that changes each time in slider
+  const [index, setIndex] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-container"></div>
+    </section>
   );
 }
 
